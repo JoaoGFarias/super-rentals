cbc06e019b1b8dd847708162cd001db3f2db5d3c
JoaoGFarias <jgfd@cin.ufpe.br>
2018-08-07 21:39:01 +0200

    Changes for blogpost

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -1,16 +1,20 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
 
 export default Controller.extend({
   actions: {
     filterByCity(param) {
-      if (param !== '') {
-        return this.get('store').query('rental', { city: param }).then((filteredResults) => {
-          return { query: param, results: filteredResults };
-        });
+      if (param !== "") {
+        return this.get("store")
+          .query("rental", { city: param })
+          .then(filteredResults => {
+            return { query: param, results: filteredResults };
+          });
       } else {
-        return this.get('store').findAll('rental').then((results) => {
-          return { query: param, results: results };
-        });
+        return this.get("store")
+          .findAll("rental")
+          .then(results => {
+            return { query: param, results: results };
+          });
       }
     }
   }
